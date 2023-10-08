b553916d504206020e64e334d6b591072c1cd02d
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-08 13:06:00 +0300

    08-10-23 Commits

--- src/main/java/com/example/lastspringbootproject007/Lastspringbootproject007Application.java
+++ src/main/java/com/example/lastspringbootproject007/Lastspringbootproject007Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lastspringbootproject007Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lastspringbootproject007Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lastspringbootproject007Application.class, args);
+    }
 
 }
